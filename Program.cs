7c4fd05f589d78a014689615ae78c60cb255979c
Thahir Hussain F <thahirhussain1414@gmail.com>
2022-04-27 21:35:33 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Management_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
